a47f9212452a27813db8c510c65907c96af1df96
Boris Smus <boris@borismus.com>
2011-06-17 13:27:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A repository of talks given by me (Boris Smus) and collaborators
---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util, template
+import os
+
+class MainHandler(webapp.RequestHandler):
+  def get(self, preso_dir):
+    base_dir = os.path.dirname(__file__)
+    full_dir = os.path.join(base_dir, 'static', preso_dir)
+    print os.path.exists(base_dir), base_dir
+    print os.path.exists(full_dir), full_dir
+    template_values = {'base_dir': self.request.path_url}
+    path = os.path.join(full_dir, 'index.html')
+    print path
+    self.response.out.write(template.render(path, template_values))
+
+
+def main():
+  application = webapp.WSGIApplication([('/(.*)', MainHandler)], debug=True)
+  util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
