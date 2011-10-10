93858cd259d50406f2ae5cb8112285682cbb22b0
Boris Smus <boris@borismus.com>
2011-10-10 11:44:23 -0700

    some changes.

---
+++ static/keynote-india-devfest-11/js/config.js
@@ -0,0 +1,19 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: false
+  },
+  info: {
+    // Personal info
+    name: 'Boris Smus',
+    pic: 'http://smus.com/media/images/self.png',
+    gplus: 'http://smus.com/+',
+    twitter: 'borismus',
+    blog: 'http://smus.com/',
+
+    // GDD info
+    location: 'Hyderabad, India',
+    date: new Date('September 17, 2011'),
+  }
+};
